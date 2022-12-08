063c5df9e3cb527e9cb349bf66ecc16ad0bb7731
kulotsystems <kulotsystems@gmail.com>
2022-12-08 10:41:01 +0800

    Setup CounterReducer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Counter } from './features/counter/Counter.jsx';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 </a>
             </div>
             <h1>Redux Toolkit Tutorial</h1>
+            <Counter/>
         </div>
     )
 }
